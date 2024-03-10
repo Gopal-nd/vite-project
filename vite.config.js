63bd1ce8c974502a769b474c2df52f757cbf7e5b
Gopal N D <gopalhelpyou@gmail.com>
2024-03-10 16:49:05 +0530

    d

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@safe-globalThis/safe-ethers-adapters',"@thirdweb-dev/react" , '@safe-globalThis/safe-core-sdk','@safe-globalThis/safe-ethers-lib']
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
